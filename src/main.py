796f4b155ad8caaa4ba40117c7fb97cd0732bf64
Falko Schindler <falko@zauberzeug.com>
2021-05-03 15:05:24 +0200

    more generic view state

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import session
 import time
 import threading
+from odrive_helpers import make_hashable, states, modes
 
 st.title("ODrive Tuning & Debugging")
 
@@ -15,67 +16,25 @@ def get_odrv():
 
 odrv = get_odrv()
 
-state = session.get_state()
-if not state.is_initialized:
-    state.is_initialized = True
-    state.axis_states = [
-        odrv.axis0.current_state,
-        odrv.axis1.current_state,
-    ]
-    state.control_modes = [
-        odrv.axis0.controller.config.control_mode,
-        odrv.axis1.controller.config.control_mode,
-    ]
-    state.vel = max(
+view = session.get_state()
+if view.vbus_voltage is None:
+    hashable = make_hashable(odrv)
+    for key in dir(hashable):
+        if not key.startswith('_'):
+            setattr(view, key, getattr(hashable, key))
+    view.vel = max(
         abs(odrv.axis0.controller.input_vel),
         abs(odrv.axis1.controller.input_vel),
     )
-    state.input_vels = [
-        odrv.axis0.controller.input_vel,
-        odrv.axis1.controller.input_vel,
-    ]
-    state.pos_gains = [
-        odrv.axis0.controller.config.pos_gain,
-        odrv.axis1.controller.config.pos_gain,
-    ]
-    state.vel_gains = [
-        odrv.axis0.controller.config.vel_gain,
-        odrv.axis1.controller.config.vel_gain,
-    ]
-    state.vel_integrator_gains = [
-        odrv.axis0.controller.config.vel_integrator_gain,
-        odrv.axis1.controller.config.vel_integrator_gain,
-    ]
-    state.vel_limits = [
-        odrv.axis0.controller.config.vel_limit,
-        odrv.axis1.controller.config.vel_limit,
-    ]
-
-states = [
-    'undefined',
-    'idle',
-    'startup sequence',
-    'full calibration sequence',
-    'motor calibration',
-    'sensorless control',
-    'encoder index search',
-    'encoder offset calibration',
-    'closed loop control',
-    'lockin spin',
-    'encoder dir find',
-    'homing',
-]
-
-modes = [
-    'voltage control',
-    'torque control',
-    'velocity control',
-    'position control',
-]
 
 cols = st.beta_columns(2)
 
 for a, axis in enumerate([odrv.axis0, odrv.axis1]):
+
+    view_axis = view.axis0 if a == 0 else view.axis1
+    view_controller = view_axis.controller
+    view_config = view_controller.config
+    odrv_config = axis.controller.config
 
     with cols[a]:
 
@@ -86,30 +45,30 @@ for a, axis in enumerate([odrv.axis0, odrv.axis1]):
 
         st.subheader('Settings')
 
-        state.pos_gains[a] = axis.controller.config.pos_gain = st.number_input('pos_gain', min_value=0.0, value=state.pos_gains[a], key=f'number-pos_gain-{a}')
-        state.vel_gains[a] = axis.controller.config.vel_gain = st.number_input('vel_gain', min_value=0.0, value=state.vel_gains[a], key=f'number-vel_gain-{a}')
-        state.vel_integrator_gains[a] = axis.controller.config.vel_integrator_gain = st.number_input('vel_integrator_gain', min_value=0.0, value=state.vel_integrator_gains[a], key=f'number-vel_integrator_gain-{a}')
-        state.vel_limits[a] = axis.controller.config.vel_limit = st.number_input('vel_limit', min_value=0.0, value=state.vel_limits[a], key=f'number-vel_limit-{a}')
+        view_config.pos_gain = odrv_config.pos_gain = st.number_input('pos_gain', min_value=0.0, value=odrv_config.pos_gain, key=f'number-pos_gain-{a}')
+        view_config.vel_gain = odrv_config.vel_gain = st.number_input('vel_gain', min_value=0.0, value=odrv_config.vel_gain, key=f'number-vel_gain-{a}')
+        view_config.vel_integrator = odrv_config.vel_integrator_gain = st.number_input('vel_integrator_gain', min_value=0.0, value=odrv_config.vel_integrator_gain, key=f'number-vel_integrator_gain-{a}')
+        view_config.vel_limit = odrv_config.vel_limit = st.number_input('vel_limit', min_value=0.0, value=odrv_config.vel_limit, key=f'number-vel_limit-{a}')
 
         st.subheader('Test')
 
-        state.control_modes[a] = axis.controller.config.control_mode = modes.index(st.radio('Mode', modes, state.control_modes[a], key=f'selectbox-mode-{a}'))
+        view_config.control_mode = odrv_config.control_mode = modes.index(st.radio('Mode', modes, odrv_config.control_mode, key=f'selectbox-mode-{a}'))
 
-        if modes[state.control_modes[a]] == 'velocity control':
-            st.write('input_vel:', state.input_vels[a])
-            state.vel = st.number_input('new value', min_value=0.0, value=state.vel, key=f'number-vel-{a}')
-            if st.button(f'Set input_vel = {-state.vel}', key=f'button-vel-{a}a'):
-                state.input_vels[a] = axis.controller.input_vel = -state.vel
+        if modes[view_config.control_mode] == 'velocity control':
+            st.write('input_vel:', view_controller.input_vel)
+            view.vel = st.number_input('new value', min_value=0.0, value=view.vel, key=f'number-vel-{a}')
+            if st.button(f'Set input_vel = {-view.vel}', key=f'button-vel-{a}a'):
+                view_controller.input_vel = axis.controller.input_vel = -view.vel
             if st.button('Set input_vel = 0.0', key=f'button-vel-{a}b'):
-                state.input_vels[a] = axis.controller.input_vel = 0.0
-            if st.button(f'Set input_vel = {state.vel}', key=f'button-vel-{a}c'):
-                state.input_vels[a] = axis.controller.input_vel = state.vel
+                view_controller.input_vel = axis.controller.input_vel = 0.0
+            if st.button(f'Set input_vel = {view.vel}', key=f'button-vel-{a}c'):
+                view_controller.input_vel = axis.controller.input_vel = view.vel
 
-        state.axis_states[a] = axis.requested_state = states.index(st.selectbox('State', states, state.axis_states[a], key=f'selectbox-state-{a}'))
-        for axis_state in ['idle', 'closed loop control']:
-            if states[state.axis_states[a]] != axis_state:
-                if st.button(f'Switch to "{axis_state}" state', key=f'button-{axis_state}-{a}'):
-                    state.axis_states[a] = states.index(axis_state)
+        view_axis.current_state = axis.requested_state = states.index(st.selectbox('State', states, view_axis.current_state, key=f'selectbox-state-{a}'))
+        for state in ['idle', 'closed loop control']:
+            if states[view_axis.current_state] != state:
+                if st.button(f'Switch to "{state}" state', key=f'button-{state}-{a}'):
+                    view_axis.current_state = states.index(state)
 
 st.sidebar.header('ODrive')
 
@@ -132,22 +91,22 @@ def update():
     print('start thread', threading.current_thread().name)
     while threading.current_thread() == [ t for t in threading.enumerate() if t.name.startswith('update') ][-1]:
         t0 = time.time()
-        while time.time() < t0 + 0.5:
-            state.data['vbus'].append(odrv.vbus_voltage)
+        while time.time() < t0 + 1.0:
+            view.data['vbus'].append(odrv.vbus_voltage)
             time.sleep(0.1)
-        state.data['time'] = time.time()
-        state.data['vbus'] = state.data['vbus'][-20:]
-        state.sync()
+        view.data['time'] = time.time()
+        view.data['vbus'] = view.data['vbus'][-20:]
+        view.sync()
     print('stop thread', threading.current_thread().name)
 
-if state.data is None:
-    state.data = {
+if view.data is None:
+    view.data = {
         'time': 0,
         'vbus': [],
     }
     thread = threading.Thread(target=update, daemon=True, name='update_%.3f' % time.time())
     thread.start()
 
-st.sidebar.json(state.data)
+st.sidebar.json(view.data)
 
-state.sync()
+view.sync()
---
+++ src/odrive_helpers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import fibre
+
+def make_hashable(obj):
+
+    d = {}
+    for key, value in obj._remote_attributes.items():
+        if type(value) == fibre.remote_object.RemoteObject:
+            d[key] = make_hashable(value)
+        if type(value) == fibre.remote_object.RemoteProperty:
+            d[key] = value.get_value()
+
+    return type('', (), d)()
+
+states = [
+    'undefined',
+    'idle',
+    'startup sequence',
+    'full calibration sequence',
+    'motor calibration',
+    'sensorless control',
+    'encoder index search',
+    'encoder offset calibration',
+    'closed loop control',
+    'lockin spin',
+    'encoder dir find',
+    'homing',
+]
+
+modes = [
+    'voltage control',
+    'torque control',
+    'velocity control',
+    'position control',
+]
+

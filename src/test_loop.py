3e747e0c1056e60a76e24b85ec7ab5ccc398d60a
Falko Schindler <falko@zauberzeug.com>
2021-05-14 07:31:47 +0200

    start re-implementation using NiceGUI

--- src/test_loop.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import streamlit as st
-import time
-
-time_view = st.json({})
-while True:
-    print(time.time())
-    time_view.json({'t': time.time()})
-    time.sleep(1)
